bb4a266193b9d8539949631a320a385726348b78
sacchy <kyonnkunn@hotmail.co.jp>
2013-06-23 04:12:36 +0900

    Initial Commit

---
+++ DragSprite/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DragSprite
+//
+//  Created by Sacchy on 2013/06/23.
+//  Copyright Sacchy 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
